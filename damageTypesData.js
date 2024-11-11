8f6905536f7e3421bee5cc81c68b6a6a22999344
MastersoftheRealm <RealmsRoleplayGame@gmail.com>
2024-11-10 19:33:44 -0700

    Create damageTypesData.js

---
+++ damageTypesData.js
@@ -0,0 +1,17 @@
+const damageTypeValues = {
+    magic: { dieBase: 2, dieIncrease: 1.5 },
+    fire: { dieBase: 2, dieIncrease: 1 },
+    ice: { dieBase: 2, dieIncrease: 1 },
+    lightning: { dieBase: 2, dieIncrease: 1 },
+    spiritual: { dieBase: 3, dieIncrease: 1.5 },
+    sonic: { dieBase: 3, dieIncrease: 1.5 },
+    poison: { dieBase: 2, dieIncrease: 1 },
+    necrotic: { dieBase: 2, dieIncrease: 1 },
+    acid: { dieBase: 2, dieIncrease: 1 },
+    psychic: { dieBase: 4, dieIncrease: 1.5 },
+    blunt: { dieBase: 2, dieIncrease: 1 },
+    piercing: { dieBase: 2, dieIncrease: 1 },
+    slashing: { dieBase: 2, dieIncrease: 1 }
+};
+
+export default damageTypeValues;
